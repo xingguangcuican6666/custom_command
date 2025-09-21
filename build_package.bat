7e6035dfe63f4cecc4f8b24affec471f2691bab8
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-09-21 19:45:34 +0800

    upload

--- build_package.bat
+++ build_package.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 mkdir dist
 copy /Y .\temp\main.exe .\dist\ctcmd.exe
-curl https://raw.githubusercontent.com/xingguangcuican6666/ctcmd_repo/refs/heads/main/module/module_x86-64_latest.zip -o .\temp\temp.zip
+curl https://raw.githubusercontent.com/xingguangcuican6666/ctcmd_repo/refs/heads/main/module/module_x86-64_latest_full.zip -o .\temp\temp.zip
 "C:\Program Files\ctcmd\module\tools\7z.exe" x .\temp\temp.zip -o.\dist\
 del .\temp\temp.zip
 cd dist
